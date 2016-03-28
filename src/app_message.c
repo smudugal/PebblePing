2b0b4f9c9f109d8dc577071a00500336868d23b5
Samyukta Mudugal <samyukta.11@gmail.com>
2016-03-27 19:05:50 -0600

    Removes unused handlers
    Signed-off-by: Warren Fernandes <warren.f.fernandes@gmail.com>

--- src/app_message.c
+++ src/app_message.c
@@ -1,13 +1,11 @@
 #include <pebble.h>
 
-static Window *s_window;	
+static Window *s_window;
 static TextLayer *s_text;
-	
+
 // Keys for AppMessage Dictionary
 // These should correspond to the values you defined in appinfo.json/Settings
 enum {
-	STATUS_KEY = 0,	
-	MESSAGE_KEY = 1,
   PING_KEY = 2
 };
 
@@ -16,23 +14,14 @@ const int PING_VALUE = 2;
 // Write message to buffer & send
 static void send_message(void){
 	DictionaryIterator *iter;
-	
+
 	app_message_outbox_begin(&iter);
 	dict_write_int(iter, PING_KEY, &PING_VALUE, sizeof(int), true);
-	
+
 	dict_write_end(iter);
   app_message_outbox_send();
 }
 
-// Called when a message is received from PebbleKitJS
-static void in_received_handler(DictionaryIterator *received, void *context) {
-}
-
-// Called when an incoming message from PebbleKitJS is dropped
-static void in_dropped_handler(AppMessageResult reason, void *context) {	
-}
-
-// Called when PebbleKitJS does not acknowledge receipt of a message
 static void out_failed_handler(DictionaryIterator *failed, AppMessageResult reason, void *context) {
   text_layer_set_text(s_text, "Ping failed!");
   APP_LOG(APP_LOG_LEVEL_ERROR, "Fail reason: %d", (int)reason);
@@ -46,14 +35,14 @@ static void out_sent_handler(DictionaryIterator *iterator, void *context){
 static void main_window_load(Window *window){
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
-  
+
   const GEdgeInsets text_insets = GEdgeInsets((bounds.size.h - 20)/2 , 0);
-  
+
   s_text = text_layer_create(grect_inset(bounds, text_insets));
   text_layer_set_text(s_text, "Ping Phone");
   text_layer_set_text_alignment(s_text, GTextAlignmentCenter);
-  
-  layer_add_child(window_layer, text_layer_get_layer(s_text));  
+
+  layer_add_child(window_layer, text_layer_get_layer(s_text));
 }
 
 static void main_window_unload(Window *window) {
@@ -77,10 +66,8 @@ static void init(void) {
   });
   window_set_click_config_provider(s_window, click_config_provider);
 	window_stack_push(s_window, true);
-	
+
 	// Register AppMessage handlers
-	app_message_register_inbox_received(in_received_handler); 
-	app_message_register_inbox_dropped(in_dropped_handler); 
 	app_message_register_outbox_failed(out_failed_handler);
   app_message_register_outbox_sent(out_sent_handler);
 
